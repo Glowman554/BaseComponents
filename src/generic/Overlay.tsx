42cf22fd848f6352e1bf6cebf885c5ca1f7c1523
Glowman554 <vossjanick62@gmail.com>
2024-12-18 22:25:28 +0100

    fix overlay on mobile

--- src/generic/Overlay.tsx
+++ src/generic/Overlay.tsx
@@ -9,7 +9,7 @@ export default function (props: Props) {
     return (
         <Show when={props.visible}>
             <div class="fixed inset-0 h-full w-full bg-slate-800/60">
-                <div class="fixed inset-0 m-auto h-fit w-1/2">{props.children}</div>
+                <div class="fixed inset-0 m-auto h-fit w-1/2 max-sm:w-4/5">{props.children}</div>
             </div>
         </Show>
     );
