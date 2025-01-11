14f14b89f14a9c0aa98ac1c08713d3e975e3875b
Glowman554 <vossjanick62@gmail.com>
2025-01-11 02:19:36 +0100

    added optional progress bar

--- src/loading/Loading.tsx
+++ src/loading/Loading.tsx
@@ -5,29 +5,48 @@ import ErrorComponent from './ErrorComponent';
 export interface LoadingInterface {
     setLoading: (loading: boolean) => void;
     setError: (error: string) => void;
+    setProgress: (progress: number) => void;
 }
 
 export const LoadingContext = createContext<LoadingInterface>({
     setLoading: (loading: boolean) => console.warn('setLoading', loading),
     setError: (error: string) => console.warn('setError', error),
+    setProgress: (progress: number) => console.warn('setProgress', progress),
 });
 
 export interface Props {
     children: JSX.Element;
     initial: boolean;
+    progressBar?: boolean;
 }
 
 export default function (props: Props) {
-    const [loading, setLoading] = createSignal(props.initial);
+    const [loading, setLoadingRaw] = createSignal(props.initial);
+    const [progress, setProgress] = createSignal(0);
     const [error, setError] = createSignal<string>();
 
+    const setLoading = (loading: boolean) => {
+        setLoadingRaw(loading);
+        if (!loading) {
+            setProgress(0);
+        }
+    };
+
     return (
-        <LoadingContext.Provider value={{ setLoading, setError }}>
+        <LoadingContext.Provider value={{ setLoading, setError, setProgress }}>
             <Show
                 when={error()}
                 fallback={
                     <>
-                        <LoadingSpinner visible={loading()} />
+                        <LoadingSpinner visible={loading()}>
+                            <Show when={props.progressBar}>
+                                <div class="h-3 w-[10vw] rounded-full bg-gray-200">
+                                    <div class="h-3 rounded-full bg-blue-600" style={{ width: `${progress()}%` }}></div>
+                                </div>
+                                <div class="center">{Math.floor(progress())}%</div>
+                            </Show>
+                        </LoadingSpinner>
+
                         <div style={{ display: loading() ? 'none' : 'block' }}>{props.children}</div>
                     </>
                 }
--- src/loading/LoadingSpinner.tsx
+++ src/loading/LoadingSpinner.tsx
@@ -1,16 +1,19 @@
 import Overlay from '../generic/Overlay';
 
 import loadingSvg from '../assets/loading.svg?url';
+import type { JSX } from 'solid-js';
 
 export interface Props {
     visible: boolean;
+    children?: JSX.Element;
 }
 
 export default function (props: Props) {
     return (
         <Overlay visible={props.visible}>
-            <div class="center">
+            <div class="flex flex-col items-center">
                 <img src={loadingSvg} class="w-8 animate-spin" />
+                {props.children}
             </div>
         </Overlay>
     );
