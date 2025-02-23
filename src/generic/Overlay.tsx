7938b1d8ab9a51a19e9c39d1eaafb7d4d5a9abba
Glowman554 <vossjanick62@gmail.com>
2025-02-23 13:58:19 +0100

    move to tailwind 4.0 and added reset option to overlay

--- src/generic/Overlay.tsx
+++ src/generic/Overlay.tsx
@@ -3,13 +3,16 @@ import { type JSX, Show } from 'solid-js';
 export interface Props {
     children: JSX.Element;
     visible: boolean;
+    reset: () => void;
 }
 
 export default function (props: Props) {
     return (
         <Show when={props.visible}>
-            <div class="fixed inset-0 h-full w-full bg-zinc-800/60">
-                <div class="fixed inset-0 m-auto h-fit w-1/2 max-sm:w-4/5">{props.children}</div>
+            <div class="fixed inset-0 h-full w-full bg-zinc-800/60" onClick={props.reset}>
+                <div class="fixed inset-0 m-auto h-fit w-1/2 max-sm:w-4/5" onClick={(e) => e.stopPropagation()}>
+                    {props.children}
+                </div>
             </div>
         </Show>
     );
